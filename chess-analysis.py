3eaeaf714df4c3c6e574532d42a9332b584febd3
Fraser Parlane <mail@fraserparlane.com>
2022-09-07 15:31:36 -0700

    First outline.

---
+++ chess-analysis.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import berserk
+
+
+def get_top_players():
+    """
+    Get a list of the top players whose games should be scraped.
+    :return: None
+    """
+
