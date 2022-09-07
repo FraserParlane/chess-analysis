a2916121dde934bb202b75849ff7ba9a2ad11639
Fraser Parlane <mail@fraserparlane.com>
2022-09-07 15:46:01 -0700

    get_top_players added.

--- chess-analysis.py
+++ chess-analysis.py
@@ -1,12 +1,24 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
 import berserk
+import json
+
+# Create a lichess session
+with open('lichess.token') as f:
+    token = f.read()
+session = berserk.TokenSession(token)
+client = berserk.Client(session)
 
 
-def get_top_players():
+def get_top_players(n: int = 10):
     """
     Get a list of the top players whose games should be scraped.
+    :param n: number of leaders to get
     :return: None
     """
 
+    leaders = client.users.get_leaderboard('classical', count=10)
+    with open('leaders.json', 'w') as f:
+        json.dump(leaders, f)
+
+
+if __name__ == '__main__':
+    get_top_players()
