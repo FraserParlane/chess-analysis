af82c1c4f47fe882b3254f1cff6851df1aaba50d
Fraser Parlane <mail@fraserparlane.com>
2022-09-08 14:20:57 -0700

    Basic plotting.

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
+import matplotlib.patches as mpatches
 from typing import Optional, List
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
+from matplotlib import cm
 import pandas as pd
 import numpy as np
 
@@ -10,19 +11,49 @@ df = pd.read_feather('plays.feather')
 
 def generic_heat_plot(
         n: np.ndarray,
+        name: str,
 ):
     """
     Make a generic heat plot of the board.
     :param n: 8x8 array
+    :param name: name of the figure
     :return: None
     """
 
+    # Formatting
+    width = 0.9
+    cmap = cm.get_cmap('viridis')
+
     # Make figure objects
-    figure: plt.Figure = plt.figure()
+    figure: plt.Figure = plt.figure(
+        dpi=300,
+        figsize=(5, 5)
+    )
     ax: plt.Axes = figure.add_subplot()
 
+    # Normalize
+    nn = n / n.max()
 
+    for i, row in enumerate(nn):
+        for j, val in enumerate(row):
+            ax.add_patch(mpatches.Rectangle(
+                (i - width / 2, j - width / 2),
+                width,
+                width,
+                color=cmap(val)
+            ))
 
+    # Format
+    ax.set_xlim(-0.5, 7.5)
+    ax.set_ylim(-0.5, 7.5)
+    ax.set_aspect('equal')
+    ax.set_xticks(range(8))
+    ax.set_xticklabels(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'])
+    ax.set_yticks(range(8))
+    ax.set_yticklabels(range(1, 9))
+
+    # Save
+    figure.savefig(f'figures/{name}.png')
 
 def plot_heat(
         white: Optional[bool] = None,
@@ -53,6 +84,7 @@ def plot_heat(
     # Plot
     generic_heat_plot(
         n=count,
+        name='test'
     )
 
 
@@ -60,7 +92,7 @@ if __name__ == '__main__':
     plot_heat(
         fast=True,
         white=True,
-        pieces=['P']
+        pieces=None,
     )
 
 
